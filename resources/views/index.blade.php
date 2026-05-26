2786ed970aef2566219f32dbf23bf6f0c967fd81
AngeArsene <nkenmandenga@gmail.com>
2026-05-26 07:46:24 +0100

    feat: Implement Chirp model and controller with resource methods

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 <x-layouts.main>
     <div class="max-w-2xl mx-auto">
-        @foreach ($chirps as $chirp)
+        @forelse ($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
@@ -12,6 +12,8 @@
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <div class="text-center mt-8 text-gray-500">No chirps yet. Be the first to chirp!</div>
+        @endforelse
     </div>
 </x-layouts.main>
