30cf0078c43de02947836f085bca0ae530104225
AngeArsene <nkenmandenga@gmail.com>
2026-05-27 13:57:13 +0100

    feat: Implement chirp listing in ChirpController and create new view for chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -12,7 +12,25 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
+        $chirps = [
+            [
+                'author' => 'Jane Doe',
+                'message' => 'Just deployed my first Laravel app! 🚀',
+                'time' => '5 minutes ago'
+            ],
+            [
+                'author' => 'John Smith',
+                'message' => 'Laravel makes web development fun again!',
+                'time' => '1 hour ago'
+            ],
+            [
+                'author' => 'Alice Johnson',
+                'message' => 'Working on something cool with Chirper...',
+                'time' => '3 hours ago'
+            ]
+        ];
+
+        return $this->resolve_view(compact('chirps'));//
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)->name('home');
-
-Route::resource('chirps', ChirpController::class);
+Route::get('/', [ChirpController::class, 'index'])->name('chirps.index');
+Route::resource('chirps', ChirpController::class)->except('index');
