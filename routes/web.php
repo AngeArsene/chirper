b903e63493770923a5aee14f914ca4d106b27a43
AngeArsene <nkenmandenga@gmail.com>
2026-07-27 22:32:48 +0100

    fix(routes): add throttling middleware for chirp resource actions

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ Route::get('/', [ChirpController::class, 'index'])->name('chirps.index');
 
 Route::resource('chirps', ChirpController::class)
     ->except('index', 'create', 'show')
-    ->middleware('auth.only');
+    ->middleware('auth.only')
+    ->middlewareFor('store', 'throttle:8,1')
+    ->middlewareFor('update', 'throttle:10,1')
+    ->middlewareFor('destroy', 'throttle:20,1');
