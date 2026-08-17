4e43f53cc94dc35628269488d6a2cbf82e17a0ee
AngeArsene <nkenmandenga@gmail.com>
2026-08-17 18:03:45 +0100

    feat(engagement-likes): add and ChirpLike Model and setup relations

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**
@@ -21,5 +22,10 @@ class Chirp extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(ChirpLike::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Chirp::class);
     }
+
+    public function chirpLikes(): HasMany
+    {
+        return $this->hasMany(ChirpLike::class);
+    }
 }
