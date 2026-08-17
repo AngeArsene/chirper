a577569761719aed28feb18d2a3955eeee7ddbbe
AngeArsene <nkenmandenga@gmail.com>
2026-08-17 18:05:34 +0100

    feat(engagement-likes): setup ChirpLike controller and factory

---
+++ app/Http/Controllers/ChirpLikeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChirpLike;
+use Illuminate\Http\Request;
+
+class ChirpLikeController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChirpLike $chirpLike)
+    {
+        //
+    }
+}
---
+++ database/factories/ChirpLikeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ChirpLike;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ChirpLike>
+ */
+class ChirpLikeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
