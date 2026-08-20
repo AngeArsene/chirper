32c92e1871106e701f1987442886ec442a360fac
AngeArsene <nkenmandenga@gmail.com>
2026-08-20 21:06:16 +0100

    refactor(ChirpLikeSeeder): refactor conditional return for zero likes in one line

--- database/seeders/ChirpLikeSeeder.php
+++ database/seeders/ChirpLikeSeeder.php
@@ -18,9 +18,7 @@ class ChirpLikeSeeder extends Seeder
         Chirp::each(function (Chirp $chirp) use ($users) {
             $count = random_int(0, $users->count());
 
-            if ($count === 0) {
-                return;
-            }
+            if ($count === 0) return;
 
             $randomUsers = $users->random($count);
 
