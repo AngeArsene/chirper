247ead53fe452cb29b52f890a711b77839738fa5
AngeArsene <nkenmandenga@gmail.com>
2026-08-22 16:41:06 +0100

    feat(seeder): add ChirpBookmarkSeeder for seeding bookmarks and update DatabaseSeeder

---
+++ database/seeders/ChirpBookmarkSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Chirp;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ChirpBookmarkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+
+        Chirp::each(function (Chirp $chirp) use ($users) {
+            $count = random_int(0, $users->count());
+
+            if ($count === 0) {
+                return;
+            }
+
+            $randomUsers = $users->random($count);
+
+            $randomUsers->each(function (User $randomUser) use ($chirp) {
+                $randomUser->likeChirp($chirp);
+            });
+        });
+    }
+}
