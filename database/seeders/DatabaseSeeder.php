f80b75de7bb453740edb3d6f447274bf41788823
AngeArsene <nkenmandenga@gmail.com>
2026-08-23 19:47:04 +0100

    refactor(DatabaseSeeder): replace UserSeeder with UserAndChirpSeeder and remove UserSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
+            UserAndChirpSeeder::class,
             ChirpLikeSeeder::class,
             ChirpBookmarkSeeder::class,
         ]);
