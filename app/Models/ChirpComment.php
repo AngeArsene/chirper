8ea7777ed730390c8b06a7fa72e3d9abae2921de
AngeArsene <nkenmandenga@gmail.com>
2026-08-30 15:48:53 +0100

    feat(ChirpComment): create ChirpComment model with relationships to User and Chirp

---
+++ app/Models/ChirpComment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable(['user_id', 'chirp_id', 'message'])]
+class ChirpComment extends Model
+{
+    /** @use HasFactory<\Database\Factories\ChirpCommentFactory> */
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function chirp(): BelongsTo
+    {
+        return $this->belongsTo(Chirp::class);
+    }
+}
