65b5c5fc8d8a96866ab31d7b4e152eab7f37a696
AngeArsene <nkenmandenga@gmail.com>
2026-09-02 17:59:11 +0100

    fix(EditChirp): update heading to use translation function for consistency

--- resources/views/chirps/edit.blade.php
+++ resources/views/chirps/edit.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.main>
     <div class="max-w-2xl mx-auto">
-        <h1 class="text-3xl font-bold mt-8">Edit Chirp</h1>
+        <h1 class="text-3xl font-bold mt-8">{{ __('Edit Chirp') }}</h1>
 
         <div class="card bg-base-100 mt-8">
             <div class="card-body">
