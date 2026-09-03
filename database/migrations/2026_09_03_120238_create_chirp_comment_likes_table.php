089b673ad327fd04f79d0dcf552b0aebcd028155
AngeArsene <nkenmandenga@gmail.com>
2026-09-03 13:53:21 +0100

    feat(CommentLike): create migration for chirp_comment_likes table

---
+++ database/migrations/2026_09_03_120238_create_chirp_comment_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chirp_comment_likes', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('chirp_comment_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->timestamp('created_at');
+
+            $table->unique(['chirp_comment_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chirp_comment_likes');
+    }
+};
