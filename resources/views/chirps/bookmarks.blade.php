71427e87e7a7535efdd77346536759b84a081e8a
AngeArsene <nkenmandenga@gmail.com>
2026-09-04 14:12:52 +0100

    refactor(bookmarks): remove unused 'for' property from x-feed component

--- resources/views/chirps/bookmarks.blade.php
+++ resources/views/chirps/bookmarks.blade.php
@@ -8,7 +8,6 @@
 
             <x-feed
                 :messages="$chirps"
-                :for="\App\Enums\MessageableType::Chirp"
                 baseRouteName="chirps"
                 emptyStateMessage="No bookmarks yet — tap the bookmark icon on any chirp to save it here."
             />
