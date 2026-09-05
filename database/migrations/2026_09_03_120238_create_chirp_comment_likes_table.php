0de7830da2e8a02db2781b8f2565cb679247e754
AngeArsene <nkenmandenga@gmail.com>
2026-09-05 20:53:37 +0100

    style(migration): remove unnecessary whitespace in chirp_comment_likes table creation

--- database/migrations/2026_09_03_120238_create_chirp_comment_likes_table.php
+++ database/migrations/2026_09_03_120238_create_chirp_comment_likes_table.php
@@ -13,12 +13,9 @@ return new class extends Migration
     {
         Schema::create('chirp_comment_likes', function (Blueprint $table) {
             $table->id();
-
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('chirp_comment_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-
             $table->timestamp('created_at');
-
             $table->unique(['chirp_comment_id', 'user_id']);
         });
     }
