2d361fc20c9854953c67048575e0fa68521961cf
AngeArsene <nkenmandenga@gmail.com>
2026-09-15 14:51:54 +0100

    fix(profile): remove password.confirm middleware from signout route

--- routes/profile.php
+++ routes/profile.php
@@ -24,5 +24,4 @@ Route::match(['PUT', 'PATCH'], 'password/update', [PasswordController::class, 'u
 
 // Profile signout route
 Route::delete('/', 'destroy')
-    ->name('destroy')
-    ->middleware('password.confirm');
+    ->name('destroy');
