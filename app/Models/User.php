28bf716a182c234656869719d68b6823598e8b6e
AngeArsene <nkenmandenga@gmail.com>
2026-09-15 02:29:44 +0100

    feat(profile): include avatar in fillable attributes of User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Carbon;
  * @property-read Collection<int, Chirp> $chirps
  * @property-read int|null $chirps_count
  */
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'avatar'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
