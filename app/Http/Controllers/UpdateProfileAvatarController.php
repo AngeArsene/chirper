b3734c91fa75b0e28a7d25b15bfbbf5affb6e868
AngeArsene <nkenmandenga@gmail.com>
2026-09-15 02:31:01 +0100

    feat(profile): add controller and request for updating user profile avatar

---
+++ app/Http/Controllers/UpdateProfileAvatarController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateProfileAvatarRequest;
+use App\Models\User;
+use Illuminate\Container\Attributes\CurrentUser;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+
+class UpdateProfileAvatarController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(UpdateProfileAvatarRequest $request, #[CurrentUser] User $user): RedirectResponse
+    {
+        if ($user->avatar) {
+            Storage::disk('public')->delete($user->avatar);
+        }
+
+        $filename = $user->id . '.' . $request->file('avatar')->extension();
+
+        $path = $request->file('avatar')->storeAs('avatars', $filename, 'public');
+
+        $user->update(['avatar' => $path]);
+
+        return back()->with('status', 'User avatar profile updated successfully.');
+    }
+}
