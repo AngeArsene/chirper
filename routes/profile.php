bac1371202bddde64286e19b52ce10a6aafdee84
AngeArsene <nkenmandenga@gmail.com>
2026-09-17 13:50:32 +0100

    fix(profile): restructure password edit view for improved organization

--- routes/profile.php
+++ routes/profile.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 // Profile view routes
 Route::view('/', 'profile.show')->name('show');
 Route::view('edit', 'profile.edit')->name('edit');
-Route::view('password/edit', 'profile.password')->name('password.edit');
+Route::view('password/edit', 'profile.password.edit')
+    ->name('password.edit');
 
 // Profile update routes
 Route::match(['PUT', 'PATCH'], '/update', 'update')
